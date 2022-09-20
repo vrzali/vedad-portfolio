402b4b0d53bb69fdecd7bd4edabdbf41dfaad6b6
Vadad Rzali <vedad.rzali@gmail.com>
2022-09-20 17:59:16 -0400

    fix about me section

--- src/components/About.js
+++ src/components/About.js
@@ -3,16 +3,15 @@ import coverImage from '../../src/assets/cover-image.JPG';
 
 export default function About() {
   return (
-    <section id="about" class="columns is-centered">
-      <div class="section">
-        <figure class="image is-128x128">
-          <img class="is-rounded" src={coverImage} />
-        </figure>
-        <h1 class="title">
-          Hi, I'm Vadad.   </h1>
-        <p class="subtitle">I love to build amazing
-          apps.
-        </p>
+    <section id="about" className="columns is-centered"> 
+      <div className="section has-text-centered">
+        <div className="container column has-text-centered mt-5">
+          <figure className="image is-128x128 is-inline-block">
+            <img className="is-rounded" src={coverImage} />
+          </figure>
+        </div>
+        <h1 className="title">Hi, I'm Vadad.</h1>
+        <p className="subtitle">I love to build amazing apps.</p>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
           laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
